f9a6544d5231b65a35cfd9683e1446b3ad951335
Vagner Humberto Wentz <vagnerwentz@Vagners-MacBook-Pro.local>
2023-01-13 16:54:27 -0300

    first commit

---
+++ DevFreela.Payments.API/Models/PaymentInfoInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DevFreela.Payments.API.Models
+{
+    public class PaymentInfoInputModel
+    {
+        public int ProjectId { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpiresAt { get; set; }
+        public string OwnerCreditCardName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
+
---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services;
+
+public class PaymentService : IPaymentService
+{
+    public Task<bool> ProcessPayment(PaymentInfoInputModel paymentInfoInputModel)
+    {
+        return Task.FromResult(true);
+    }
+}
+
+
